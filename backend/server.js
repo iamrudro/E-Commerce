19d8070cafc7bfff387f5865e579229891d827f7
“Rudranil <rudranil.das9900@gmail.com>
2025-04-13 12:19:00 +0530

    #7-Connecting Website to MongoDB for Seamless Data Management

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import { connectMongoDataBase } from "./config/db.js";
+
 dotenv.config({ path: 'backend/config/config.env' });
+connectMongoDataBase();
 const port = process.env.PORT || 3000;
 
 // console.log(app);
