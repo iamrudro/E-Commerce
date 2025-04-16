1faae1d18a98f0c1bd3862e5672bd97f929df6c1
“Rudranil <rudranil.das9900@gmail.com>
2025-04-16 20:50:12 +0530

    #17-Handle Backend Uncaught Exception Errors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import { connectMongoDataBase } from "./config/db.js";
-
 dotenv.config({ path: 'backend/config/config.env' });
 connectMongoDataBase();
+
+// Handle Uncaught Exception Errors
+process.on('uncaughtException', (err) => {
+    console.log(`Error:${err.message}`);
+    console.log(`Server is shutting down, due to unhandled Exception Errors`);
+    process.exit(1);
+})
+
 const port = process.env.PORT || 3000;
 
 // console.log(app);
