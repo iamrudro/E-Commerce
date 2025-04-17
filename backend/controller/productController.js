385f1871ad886dad9abec8e90309110671cf8ae5
“Rudranil <rudranil.das9900@gmail.com>
2025-04-17 11:33:27 +0530

    #19-Search Functionality

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,6 +1,11 @@
 import Product from "../models/productModel.js";
 import HandleError from "../utils/handleError.js";
 import handleAsyncError from "../middleware/handleAsyncError.js";
+import APIFunctionality from "../utils/apiFunctionality.js";
+
+
+// http://localhost:8000/api/v1/product/67fb9a0ebba9a69207d603da?keyword=shirt
+
 
 // 1.Creating Products
 export const createProducts = handleAsyncError(async (req, res, next) => {
@@ -15,7 +20,8 @@ export const createProducts = handleAsyncError(async (req, res, next) => {
 
 // 2.Getting All Products
 export const getAllProducts = handleAsyncError(async (req, res, next) => {
-    const products = await Product.find()
+    const apiFunctionality = new APIFunctionality(Product.find(), req.query).search();
+    const products = await apiFunctionality.query
     res.status(200).json({
         // message: "All Products"
         success: true,
