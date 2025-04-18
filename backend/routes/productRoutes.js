2fc8ce3321dab49bdaca2589401e01cb577b3134
“Rudranil <rudranil.das9900@gmail.com>
2025-04-18 17:25:11 +0530

    #31-Verifying User Authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createProducts, deleteProduct, getAllProducts, getSingleProduct, updateProduct } from '../controller/productController.js';
+import { verifyUserAuth } from '../middleware/userAuth.js';
 const router = express.Router();
 
 // Routes
 router.route("/products")
-    .get(getAllProducts)
+    .get(verifyUserAuth, getAllProducts)
     .post(createProducts);
 
 router.route("/product/:id")
