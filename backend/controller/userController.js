c682846624b62471d1c286cf228f81420ebe03a9
“Rudranil <rudranil.das9900@gmail.com>
2025-04-19 21:38:16 +0530

    #37-Generate Reset URL for Password Reset

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -79,7 +79,16 @@ export const requestPasswordReset = handleAsyncError(async (req, res, next) => {
         resetToken = user.generatePasswordResetToken()
         await user.save({ validateBeforeSave: false })
     } catch (error) {
-        console.log(error)
         return next(new HandleError("Could not save reset token,Please try again later", 500))
+    }
+    const resetPasswordURL = `http://localhost/api/v1/reset/${resetToken}`;
+    const message = `Use the follwoing link to reset your password: ${resetPasswordURL}. \n\n This link will expire in 30 minutes. \n\n If you didn't request a password reset , please ignore this message.`;
+    try {
+
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+        await user.save({ validateBeforeSave: false })
+        return next(new HandleError("Email could not be sent, Please try again later", 500))
     }
 })
