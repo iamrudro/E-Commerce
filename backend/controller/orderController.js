2d4674df58d0271a7f48f6cb04b9e7e3c9d1bbf3
“Rudranil <rudranil.das9900@gmail.com>
2025-04-26 00:43:19 +0530

    #57-Get Logged-In User Orders

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -37,3 +37,16 @@ export const getSingleOrder = handleAsyncError(async (req, res, next) => {
         order
     })
 })
+
+
+// 3️⃣. All my orders
+export const allMyOrders = handleAsyncError(async (req, res, next) => {
+    const orders = await Order.find({ user: req.user._id });
+    if (!orders) {
+        return next(new HandleError("No order found", 404));
+    }
+    res.status(200).json({
+        success: true,
+        orders
+    })
+})
