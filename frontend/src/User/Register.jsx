d2b1cf95e21b2a4a6de61cead7cab60bf61e651d
“Rudranil <rudranil.das9900@gmail.com>
2025-05-22 09:31:28 +0530

    #88-Registration Form - onChange Event Handler

--- frontend/src/User/Register.jsx
+++ frontend/src/User/Register.jsx
@@ -13,23 +13,38 @@ function Register() {
     const [avatarPreview, setAvatarPreview] = useState('./images/avatar.jpg')
     const { name, email, password } = user;
 
+    const registerDataChange = (e) => {
+        if (e.target.name === 'avatar') {
+            const reader = new FileReader();
+            reader.onload = () => {
+                if (reader.readyState === 2) {
+                    setAvatarPreview(reader.result)
+                    setAvatar(reader.result)
+                }
+            }
+            reader.readAsDataURL(e.target.files[0]);
+        } else {
+            setUser({ ...user, [e.target.name]: e.target.value })
+        }
+    }
+
     return (
         <div className="form-container container">
             <div className="form-content">
                 <form className="form">
                     <h2>Sign Up</h2>
                     <div className="input-group">
-                        <input type="text" placeholder='Username' name="name" value={name} />
+                        <input type="text" placeholder='Username' name="name" value={name} onChange={registerDataChange} />
                     </div>
                     <div className="input-group">
-                        <input type="email" placeholder='Email' name="email" value={email} />
+                        <input type="email" placeholder='Email' name="email" value={email} onChange={registerDataChange} />
                     </div>
                     <div className="input-group">
-                        <input type="password" placeholder='Password' name="password" value={password} />
+                        <input type="password" placeholder='Password' name="password" value={password} onChange={registerDataChange} />
                     </div>
                     <div className="input-group avatar-group">
-                        <input type="file" name="avatar" className='file-input' accept='image/' />
-                        <img src={avatarPreview} alt="Avatar Preview" className='avatar'/>
+                        <input type="file" name="avatar" className='file-input' accept='image/' onChange={registerDataChange} />
+                        <img src={avatarPreview} alt="Avatar Preview" className='avatar' />
                     </div>
                     <button className="authBtn">Sign Up</button>
                     <p className="form-links">
