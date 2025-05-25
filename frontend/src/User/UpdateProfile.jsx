6afd7acfd600fc27266aca3426b4b0ccdddc5bd7
“Rudranil <rudranil.das9900@gmail.com>
2025-05-25 17:55:41 +0530

    #106-Working on Profile Image Update

--- frontend/src/User/UpdateProfile.jsx
+++ frontend/src/User/UpdateProfile.jsx
@@ -2,12 +2,27 @@ import React, { useState } from 'react';
 import '../UserStyles/Form.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { toast } from 'react-toastify';
 
 function UpdateProfile() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [avatar, setAvatar] = useState("");
     const [avatarPreview, setAvatarPreview] = useState("./images/avatar.jpg");
+
+    const profileImageUpdate = (e) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            if (reader.readyState === 2) {
+                setAvatarPreview(reader.result)
+                setAvatar(reader.result)
+            }
+        }
+        reader.onerror = (error) => {
+            toast.error('Error Reading file')
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    }
 
     return (
         <>
@@ -17,7 +32,7 @@ function UpdateProfile() {
                     <form className="form">
                         <h2>Update Profile</h2>
                         <div className="input-group avatar-group">
-                            <input type="file" accept="image/" className="file-input" />
+                            <input type="file" accept="image/" className="file-input" onChange={profileImageUpdate} />
                             <img src={avatarPreview} alt="User Profile" className="avatar" />
                         </div>
                         <div className="input-group">
