d27237e82df97956ec3cf4ce78ecde9822aa5a32
“Rudranil <rudranil.das9900@gmail.com>
2025-05-25 17:18:59 +0530

    #103-Render Logged in User Information

--- frontend/src/User/Profile.jsx
+++ frontend/src/User/Profile.jsx
@@ -1,34 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../UserStyles/Profile.css';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import PageTitle from '../components/PageTitle';
+import Loader from '../components/Loader';
 
 function Profile() {
+
+    const { loading, isAuthenticated, user } = useSelector(state => state.user)
+    console.log(user);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (isAuthenticated === false) {
+            navigate("/login")
+        }
+    }, [isAuthenticated])
+
     return (
-        <div className="profile-container">
-            <div className="profile-image">
-                <h1 className="profile-heading">My Profile</h1>
-                <img src="" alt="User Profile" className="profile-image" />
-                <Link to="/password/update">Edit Profile</Link>
-            </div>
-            <div className="profile-details">
-                <div className="profile-detail">
-                    <h2>Username:</h2>
-                    <p>Rudranil</p>
+        <>
+            {loading ? (<Loader />) : (<div className="profile-container">
+                <PageTitle title={`${user.name} Profile`} />
+                <div className="profile-image">
+                    <h1 className="profile-heading">My Profile</h1>
+                    <img src={user.avatar.url ? user.avatar.url : './images/avatar.jpg'} alt="User Profile" className="profile-image" />
+                    <Link to="/password/update">Edit Profile</Link>
                 </div>
-                <div className="profile-detail">
-                    <h2>Email:</h2>
-                    <p>rudranil.das9900@gmail.xom</p>
+                <div className="profile-details">
+                    <div className="profile-detail">
+                        <h2>Username:</h2>
+                        <p>{user.name}</p>
+                    </div>
+                    <div className="profile-detail">
+                        <h2>Email:</h2>
+                        <p>{user.email}</p>
+                    </div>
+                    <div className="profile-detail">
+                        <h2>Joined on:</h2>
+                        <p>{user.createdAt ? String(user.createdAt).substring(0, 10) : 'N/A'}</p>
+                    </div>
                 </div>
-                <div className="profile-detail">
-                    <h2>Joined on:</h2>
-                    <p>23/05/2025</p>
+                <div className="profile-buttons">
+                    <Link to="/orders/user">My Orders</Link>
+                    <Link to="/password/update">Change Password</Link>
                 </div>
-            </div>
-            <div className="profile-buttons">
-                <Link to="/orders/user">My Orders</Link>
-                <Link to="/password/update">Change Password</Link>
-            </div>
-        </div>
+            </div>)}
+        </>
     )
 }
 
