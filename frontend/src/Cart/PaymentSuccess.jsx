f32ad0243c5c540b13166abda829f1237bb3cdd5
“Rudranil <rudranil.das9900@gmail.com>
2025-06-04 12:24:39 +0530

    #144-Designing the Payment Success Page

--- frontend/src/Cart/PaymentSuccess.jsx
+++ frontend/src/Cart/PaymentSuccess.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../CartStyles/PaymentSuccess.css';
+import { Link, useSearchParams } from 'react-router-dom';
 
 function PaymentSuccess() {
-  return (
-    <div>
-      confirm
-    </div>
-  )
+    const [searchParams] = useSearchParams()
+    const reference = searchParams.get('reference')
+    return (
+        <div className="payment-success-container">
+            <div className="success-icon">
+                <div className="checkmark"></div>
+            </div>
+            <h1>Order Confirmed</h1>
+            <p>Your Payment was successfull. Reference ID <strong>{reference}</strong></p>
+            <Link className='explore-btn' to="/">Explore more Products</Link>
+        </div>
+    )
 }
 
 export default PaymentSuccess
